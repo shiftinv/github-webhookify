74b340c56771d02696f68018d7fbfbc06fa58fef
shiftinv <me@shiftinv.cc>
2025-09-21 11:54:50 +0200

    feat: add etag support

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { request as githubRequest } from "@octokit/request";
+import { RequestError } from "@octokit/request-error";
 
 import { convertPushEvent, PartialWebhookPushEvent } from "./convert.ts";
 import env from "./env.ts";
@@ -6,17 +7,34 @@ import type PushEvent from "./pushEvent.d.ts";
 import { initSentry } from "./sentry.ts";
 
 const KV_KEY_LAST_UPDATE = ["last-update"];
+const KV_KEY_ETAG = ["last-etag"];
 const kv = await Deno.openKv(Deno.env.get("KV_PATH"));
 
 async function checkGitHub(): Promise<void> {
     const lastId = (await kv.get<number>(KV_KEY_LAST_UPDATE)).value ?? 0;
+    const etag = (await kv.get<string>(KV_KEY_ETAG)).value || undefined;
 
-    const events = await githubRequest("GET /repos/{owner}/{repo}/events", {
-        owner: "DisnakeDev",
-        repo: "disnake",
-        // I don't expect more than 100 new events to occur within 5 minutes for now, to keep things simple
-        per_page: 100,
-    });
+    let events: Awaited<ReturnType<typeof githubRequest<"GET /repos/{owner}/{repo}/events">>>;
+    try {
+        events = await githubRequest("GET /repos/{owner}/{repo}/events", {
+            owner: "DisnakeDev",
+            repo: "disnake",
+            // I don't expect more than 100 new events to occur within 5 minutes for now, to keep things simple
+            per_page: 100,
+            headers: { "If-None-Match": etag },
+        });
+    } catch (e) {
+        if (e instanceof RequestError && e.status == 304) {
+            console.debug("received 304");
+            return;
+        }
+        throw e;
+    }
+
+    const newEtag = events.headers["etag"];
+    console.debug(`previous etag: ${etag}, new etag: ${newEtag}`);
+    if (newEtag) await kv.set(KV_KEY_ETAG, newEtag);
+
     for (const ev of events.data) {
         const id = Number(ev.id);
         if (id <= lastId) break;
